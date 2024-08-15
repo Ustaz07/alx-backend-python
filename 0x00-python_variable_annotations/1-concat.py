4b586dca597bcc755dea627adda508ee61a07222
Ustaz07 <shamsutukor@gmail.com>
2024-08-15 10:24:37 -0700

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Defines a function called 'concat'
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenation of two strings.
+    
+    Args:
+        str1 (str)
+        str2 (str)
+    
+    Return:
+        (str)
+    """
+    return str1 + str2
