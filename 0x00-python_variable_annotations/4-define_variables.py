fcfa815474bf28f798556657cb3fd93670013bdb
Ustaz07 <shamsutukor@gmail.com>
2024-08-15 10:53:22 -0700

    4-define_variables.py

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
-"""
-A module: Define and annotate variables of various types
-"""
+""" Define variables """
 
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
 school: str = "Holberton"
-
