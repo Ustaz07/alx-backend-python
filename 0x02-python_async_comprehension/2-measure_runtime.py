bac34562f1db0848cff37ddcae199edf09b9a172
Ustaz07 <shamsutukor@gmail.com>
2024-08-22 05:17:19 -0700

    2-measure_runtime.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,16 +8,17 @@ from time import time
 # Import async_comprehension from the previous task
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime() -> float:
-    """Measure the runtime for executing async_comprehension four times in parallel."""
+    """Measure runtime for exe. async_comprehension 4 times in parallel."""
     start = time()
-    
+
     # Create a list of tasks to run async_comprehension four times
     tasks = [async_comprehension() for _ in range(4)]
-    
+
     # Run the tasks in parallel and wait for all to complete
     await gather(*tasks)
-    
+
     end = time()
-    
+
     return end - start
